c42c67ca907eb16fe2a82fd04f286d0f0822d5c3
Emme Menezes <menezes.ecd@gmail.com>
2023-04-14 19:40:10 -0300

    fix: remove chakra-ui

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
-import { ChakraProvider } from "@chakra-ui/react";
-
 import { Router } from "./pages/Router";
 import { BrowserRouter } from "react-router-dom";
 
 function App() {
   return (
-    <ChakraProvider>
-      <BrowserRouter>
-        <Router />
-      </BrowserRouter>
-    </ChakraProvider>
+    <BrowserRouter>
+      <Router />
+    </BrowserRouter>
   );
 }
 
