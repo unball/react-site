e49c48cf4dcf387b2ba0b689e1010e093ac21d31
Emme Menezes <menezes.ecd@gmail.com>
2023-04-15 17:51:42 -0300

    feat: adiciona fonte

--- src/App.js
+++ src/App.js
@@ -2,11 +2,18 @@ import { ThemeProvider, createTheme } from "@mui/material";
 import { Router } from "./pages/Router";
 import { BrowserRouter } from "react-router-dom";
 
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
 const theme = createTheme({
   palette: {
+    backgroundLight: {
+      main: "#202024",
+    },
     background: {
       main: "#121214",
-      light: "#202024",
     },
     text: { main: "#D5D5DA" },
     link: { main: "#EF6F1E" },
