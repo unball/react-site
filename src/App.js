5ca43e96b23175c32a609894fe45b9a3eac20e61
Emme Menezes <menezes.ecd@gmail.com>
2023-04-14 21:17:56 -0300

    feat: adiciona paleta de cores do projeto

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
+import { ThemeProvider, createTheme } from "@mui/material";
 import { Router } from "./pages/Router";
 import { BrowserRouter } from "react-router-dom";
 
+const theme = createTheme({
+  palette: {
+    background: {
+      main: "#121214",
+      light: "#202024",
+    },
+    text: { main: "#D5D5DA" },
+    link: { main: "#EF6F1E" },
+  },
+});
+
 function App() {
   return (
-    <BrowserRouter>
-      <Router />
-    </BrowserRouter>
+    <ThemeProvider theme={theme}>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
+    </ThemeProvider>
   );
 }
 
