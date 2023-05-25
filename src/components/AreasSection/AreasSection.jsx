4797ab9354d357cbd5ee40d58f2ad001acda1833
Maria Claudia <mariaclaudia8.0@hotmail.com>
2023-05-25 17:23:38 -0300

    feat: implementa pagina de areas de atuacao

--- src/components/AreasSection/AreasSection.jsx
+++ src/components/AreasSection/AreasSection.jsx
@@ -1,9 +1,23 @@
 import React from "react";
+import { TitlePage } from "./components/TitlePage";
+import { Stack } from "@mui/material";
+import { ListAreas } from "./components/ListAreas/ListAreas";
 
 export function AreasSection() {
-  return(
-    <div id="areas">
-      <h1>Areas</h1>
-    </div>
-  )
+  return (
+    <Stack
+      id="areas"
+      sx={{
+        backgroundColor: "#121214",
+        paddingBottom: "160px",
+      }}
+      direction={{ xs: "column", sm: "column" }}
+      spacing={{ xs: 1, sm: 2, md: 4 }}
+      justifyContent="center"
+      alignItems="center"
+    >
+      <TitlePage />
+      <ListAreas />
+    </Stack>
+  );
 }
---
+++ src/components/AreasSection/Text.js
@@ -0,0 +1,8 @@
+export const TITLE = "Áreas de Atuação";
+
+// export const SUBTITLE = "Futebol de robôs";
+
+// export const DESCRIPTION =
+//   "A UnBall é uma iniciativa de alunos da Universidade de Brasília com o objetivo de desenvolver um grupo para atuar nas competições nacionais e internacionais de futebol de robôs. A equipe visa realizar o desenvolvimento de uma plataforma robótica que permita a um time de robôs jogar futebol.";
+
+// export const ALT = "Logo simplificado da UnBall, uma raposa geométrica laranja";
