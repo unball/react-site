770d0917c2b8c7bf40e18b210e587536d8f22410
Emme Menezes <menezes.ecd@gmail.com>
2023-06-04 19:21:39 -0300

    feat: adiciona cores e fontes responsivas

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import { ThemeProvider, createTheme } from "@mui/material";
+import { ThemeProvider, createTheme, responsiveFontSizes } from "@mui/material";
 import { Router } from "./pages/Router";
 import { BrowserRouter } from "react-router-dom";
 
-import '@fontsource/roboto/300.css';
-import '@fontsource/roboto/400.css';
-import '@fontsource/roboto/500.css';
-import '@fontsource/roboto/700.css';
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
 
-const theme = createTheme({
+let theme = createTheme({
   palette: {
     backgroundLight: {
       main: "#202024",
@@ -15,10 +15,39 @@ const theme = createTheme({
     background: {
       main: "#121214",
     },
-    text: { main: "#D5D5DA" },
+    text: {
+      light: "#E1E1E6",
+      main: "#D5D5DA",
+      dark: "#9C9CA1",
+    },
     link: { main: "#EF6F1E" },
   },
+  typography: {
+    h1: {
+      fontSize: "90px",
+    },
+    h2: {
+      fontSize: "90px",
+    },
+    h3: {
+      fontSize: "48px",
+    },
+    h4: {
+      fontSize: "36px",
+    },
+    h5: {
+      fontSize: "27px",
+    },
+    body1: {
+      fontSize: "24px",
+    },
+    body2: {
+      fontSize: "18px",
+    },
+  },
 });
+
+theme = responsiveFontSizes(theme);
 
 function App() {
   return (
